6b264cc1b3635ee5fefe510f1194bf9b5827c675
sai-roshan-dev <neelamsairoshan2003@gmail.com>
2025-12-12 17:51:45 +0530

    Add type declarations for Express and CORS

---
+++ practo-cms-be/src/types/express.d.ts
@@ -0,0 +1,9 @@
+// Type declarations to help TypeScript find Express types
+declare module 'express' {
+  export * from '@types/express';
+}
+
+declare module 'cors' {
+  export * from '@types/cors';
+}
+
