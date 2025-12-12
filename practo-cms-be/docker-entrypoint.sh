150ccb4f2c31ac61599ce7a687287687b58409a7
sai-roshan-dev <neelamsairoshan2003@gmail.com>
2025-12-12 17:55:27 +0530

    Update Dockerfile with entrypoint script for database migrations

---
+++ practo-cms-be/docker-entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+
+# Run database migrations
+echo "Running database migrations..."
+npx prisma migrate deploy
+
+# Start the application
+echo "Starting application..."
+exec npm start
+
